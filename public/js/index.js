d3879ca37f5e661be6db3a348a984426b3f8ffd7
saurabh <saurabh.abhyankar7@gmail.com>
2018-08-27 16:40:21 +0530

    Added emit on connect

--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,13 @@
 var socket = io();
 
 socket.on('connect', function () {
-  console.log('Connected to server');  
-});
-
-socket.emit('createMessageEvent', {
-  from: 'Saurabh',
-  text: 'Hey. This is Saurabh.'
-});
-
-socket.on('newMessageEvent', function (message) {
-  console.log('New Message', message);
+  console.log('Connected to server');
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+});
--- server/server.js
+++ server/server.js
@@ -14,10 +14,30 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.on('createMessageEvent', (newMessage) => {
-    newMessage.createdAt = Date.now();
-    console.log('createMessage', newMessage);
-    socket.emit('newMessageEvent', newMessage);
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
